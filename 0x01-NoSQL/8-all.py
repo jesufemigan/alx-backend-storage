c8bf0eb9d91fb8f02582814c47e0dff350865764
Jesufemi <oladapofemmy@gmail.com>
2024-07-15 17:01:20 +0100

    0-7

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+lists all documents in a collection
+'''
+import pymongo
+
+
+def list_all(mongo_collection):
+    '''
+    a function that lists all documents in a collection
+    '''
+    
